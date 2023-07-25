cb525e122f01c2fa61cf1a6938edd627499c9b93
Vansh Gupta <you@example.com>
2023-07-25 19:02:39 +0530

    1st

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import React, { useState } from "react";
+// import './../styles/App.css';
+import SearchBar from "./component/SearchBar";
+
+
+const App = () => {
+
+  
+  
+  return (
+    <div>
+       <SearchBar/>
+       
+    </div>
+  )
+}
+
+export default App
